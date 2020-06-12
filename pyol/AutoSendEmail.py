901b1d979e5f9591e0f62992af0c9a4fa973978b
charlielinz <65207965+charlielinz@users.noreply.github.com>
2020-06-12 16:51:58 +0800

    AUTO_SEND_EMAIL
    DONE

--- pyol/AutoSendEmail.py
+++ pyol/AutoSendEmail.py
@@ -54,10 +54,8 @@ E: charlielin@iii.org.tw
 
 """
 path = r"C:\Users\charlielin\Desktop\檔案\工作週報"
-
 a = os.listdir(path)
-a.reverse()
-location = path + "\\" + a[0]
+location = path + "\\" + a[-1]
 
 
 def mail_job():
