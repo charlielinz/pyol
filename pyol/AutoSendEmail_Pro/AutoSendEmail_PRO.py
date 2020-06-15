d486079b5929e35bd773e0448db6298643cfb1d3
charlielinz <65207965+charlielinz@users.noreply.github.com>
2020-06-15 14:25:55 +0800

    autosend_pro

--- pyol/AutoSendEmail_Pro/AutoSendEmail_PRO.py
+++ pyol/AutoSendEmail_Pro/AutoSendEmail_PRO.py
@@ -1,21 +1,18 @@
 from win32com import client
 import schedule
 import time
-import os.path
+import os
 
 
 """Define file path"""
 this_py_file_path = os.path.abspath(__file__)
 this_py_file_dir_path = os.path.dirname(this_py_file_path)
 
-config_dir_path = os.path.join(this_py_file_dir_path,"weekly_report")
-subject_path = os.path.join(config_dir_path,"subject_config.txt")
-to_path = os.path.join(config_dir_path,"to_config.txt")
-body_path = os.path.join(config_dir_path,"body_config.txt")
+config_folder_dir_path = os.path.join(this_py_file_dir_path,"config_folder")
+folder_paths = os.listdir(config_folder_dir_path)
 
-#subject_config = ""
-#to_config = ""
-#body_config = ""
+    
+
     
 def send_mail(subject, body, to, cc='', bcc='', attachments=[], just_show=False):
    
@@ -40,35 +37,51 @@ def send_mail(subject, body, to, cc='', bcc='', attachments=[], just_show=False)
         mail.Send()
 
 
-def send_mail_by_config_file(config_dir_path):
-    
-    with open(subject_path, encoding="utf-8") as file1:
-        subject = file1.readlines()
-    subject_config = subject[0]
+for folder_path in folder_paths:
 
-    with open(to_path, encoding="utf-8") as file2:
-        to = file2.readlines()
-    to_list = ";".join(to)
-    to_config = to_list.replace("\n","")
+    def send_mail_by_config_file():
 
-    with open(body_path, encoding="utf-8") as file3:
-        body = file3.readlines()
-    body_config = "".join(body)
+        config_dir_path = os.path.join(config_folder_dir_path,folder_path)
+        subject_path = os.path.join(config_dir_path,"subject_config.txt")
+        to_path = os.path.join(config_dir_path,"to_config.txt")
+        body_path = os.path.join(config_dir_path,"body_config.txt")
 
-    send_mail(subject=subject_config,body=body_config,to=to_config)
+        with open(subject_path, encoding="utf-8") as file1:
+            subject = file1.readlines()
+        subject_config = subject[0]
+
+        with open(to_path, encoding="utf-8") as file2:
+            to = file2.readlines()
+        to_list = ";".join(to)
+        to_config = to_list.replace("\n","")
+
+        with open(body_path, encoding="utf-8") as file3:
+            body = file3.readlines()
+        body_config = "".join(body)
+
+        send_mail(subject=subject_config,body=body_config,to=to_config)
         
-def mail_job():
-    send_mail_by_config_file(config_dir_path=config_dir_path)
+    config_dir_path = os.path.join(config_folder_dir_path,folder_path)
+    status_path =os.path.join(config_dir_path,"status_config.txt")
+    with open(status_path, encoding="utf-8") as file4:
+        status = file4.readlines()
+        status_config = status[0].replace("\n","")
+        period = int(status[1])
+        if status_config == "on":
+            schedule.every(period).seconds.do(send_mail_by_config_file)
 
-schedule.every(10).seconds.do(mail_job)
+while True:
+    schedule.run_pending()
+    time.sleep(10)
+
+
+
 
 """
 For more schedule example:
 https://github.com/dbader/schedule
 """
 
-while True:
-    schedule.run_pending()
-    time.sleep(10)
+
     
     
