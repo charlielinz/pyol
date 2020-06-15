5aebf9b5be937a6e95bb9a2b94adaa97d572f4bd
charlielinz <65207965+charlielinz@users.noreply.github.com>
2020-06-15 16:52:02 +0800

    autosendmail_pro

--- pyol/AutoSendEmail_Pro/AutoSendEmail_PRO.py
+++ pyol/AutoSendEmail_Pro/AutoSendEmail_PRO.py
@@ -4,20 +4,22 @@ import time
 import os
 
 
-"""Define file path"""
+"""
+Define file path
+"""
+
 this_py_file_path = os.path.abspath(__file__)
 this_py_file_dir_path = os.path.dirname(this_py_file_path)
 
-config_folder_dir_path = os.path.join(this_py_file_dir_path,"config_folder")
-folder_paths = os.listdir(config_folder_dir_path)
+config_folder_dir_path = os.path.join(this_py_file_dir_path, "config_folder")
+folder_paths_list = os.listdir(config_folder_dir_path)
 
-    
 
-    
 def send_mail(subject, body, to, cc='', bcc='', attachments=[], just_show=False):
-   
-    """The method of sending Email by outlook."""
-    
+    """
+    The method of sending Email by outlook.
+    """
+
     olMailItem = 0x0
     outlook_client = client.Dispatch("Outlook.Application")
     mail = outlook_client.CreateItem(olMailItem)
@@ -37,38 +39,40 @@ def send_mail(subject, body, to, cc='', bcc='', attachments=[], just_show=False)
         mail.Send()
 
 
-for folder_path in folder_paths:
+for folder_path in folder_paths_list:
+     
+    config_dir_path = os.path.join(config_folder_dir_path, folder_path)
+    status_config_path = os.path.join(config_dir_path, "status_config.txt")
 
-    def send_mail_by_config_file():
+    with open(status_config_path, encoding="utf-8") as status_config:
+        status_list = status_config.readlines()
+        status = status_list[0].replace("\n", "")
+        period = int(status_list[1])
+    if status == "on":
+        def send_mail_by_config_file(config_dir_path):
+              
+            subject_path = os.path.join(config_dir_path, "subject_config.txt")
+            to_path = os.path.join(config_dir_path, "to_config.txt")
+            body_path = os.path.join(config_dir_path, "body_config.txt")
 
-        config_dir_path = os.path.join(config_folder_dir_path,folder_path)
-        subject_path = os.path.join(config_dir_path,"subject_config.txt")
-        to_path = os.path.join(config_dir_path,"to_config.txt")
-        body_path = os.path.join(config_dir_path,"body_config.txt")
 
-        with open(subject_path, encoding="utf-8") as file1:
-            subject = file1.readlines()
-        subject_config = subject[0]
+            with open(subject_path, encoding="utf-8") as subject_config:
+                subject_list = subject_config.readlines()
+            subject = subject_list[0]
 
-        with open(to_path, encoding="utf-8") as file2:
-            to = file2.readlines()
-        to_list = ";".join(to)
-        to_config = to_list.replace("\n","")
+            with open(to_path, encoding="utf-8") as to_config:
+                to_list = to_config.readlines()
+            to_list_str = ";".join(to_list)
+            to = to_list_str.replace("\n", "")
 
-        with open(body_path, encoding="utf-8") as file3:
-            body = file3.readlines()
-        body_config = "".join(body)
+            with open(body_path, encoding="utf-8") as body_config:
+                body_list = body_config.readlines()
+            body = "".join(body_list)
 
-        send_mail(subject=subject_config,body=body_config,to=to_config)
-        
-    config_dir_path = os.path.join(config_folder_dir_path,folder_path)
-    status_path =os.path.join(config_dir_path,"status_config.txt")
-    with open(status_path, encoding="utf-8") as file4:
-        status = file4.readlines()
-        status_config = status[0].replace("\n","")
-        period = int(status[1])
-        if status_config == "on":
-            schedule.every(period).seconds.do(send_mail_by_config_file)
+            send_mail(subject=subject, body=body, to=to)
+
+        schedule.every(period).seconds.do(send_mail_by_config_file,config_dir_path=os.path.join(config_folder_dir_path, folder_path))
+
 
 while True:
     schedule.run_pending()
@@ -76,12 +80,7 @@ while True:
 
 
 
-
 """
 For more schedule example:
 https://github.com/dbader/schedule
 """
-
-
-    
-    
--- pyol/readingfile.py
+++ pyol/readingfile.py
@@ -1,5 +1,10 @@
-import os
-this_py_file_path = os.path.abspath(__file__)
-this_py_file_dir_path = os.path.dirname(this_py_file_path)
-config_dir_path = os.path.join(this_py_file_dir_path,"weekly_report")
-print(config_dir_path)
+functions = []
+
+for k in range(3):
+    def f(k=k):
+        return k
+
+    # alternatively: f = lambda: i
+
+    functions.append(f)
+print(f)
